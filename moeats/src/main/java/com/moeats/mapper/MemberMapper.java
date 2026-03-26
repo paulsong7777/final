2d2414ecacb78a1fe4e67af1cf9aa51b88daa88f
ysy9038-eng <ysy9038@gmail.com>
2026-03-26 18:24:06 +0900

    메인 페이지 화면 구성

---
+++ moeats/src/main/java/com/moeats/mapper/MemberMapper.java
@@ -0,0 +1,20 @@
+package com.moeats.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.javassist.compiler.ast.Member;
+
+/* @Mapper는 MyBatis 3.0부터 지원하는 애노테이션으로 이 애노테이션이 붙은
+ * 인터페이스는 별도의 구현 클래스를 작성하지 않아도 MyBatis 맵퍼로 인식해
+ * 스프링 Bean으로 등록되며 Service 클래스에서 주입 받아 사용할 수 있다. 
+ * 
+ * @Mapper 애노테이션을 적용한 인터페이스와 XML 맵퍼 파일은 namespace라는
+ * 속성으로 연결되기 때문에 XML 맵퍼 파일의 namespace를 정의할 때 맵퍼 
+ * 인터페이스의 완전한 클래스 이름과 동일한 namespace를 사용해야 한다. 
+ **/
+@Mapper
+public interface MemberMapper {
+
+   // 회원 ID에 해당하는 회원 정보를 member 테이블에서 읽어와 반환하는 메서드
+   public Member getMember(String id);   
+}
+
