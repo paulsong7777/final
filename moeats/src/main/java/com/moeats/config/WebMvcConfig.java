cafe4b4b349b1169493ef1cbbf50fa71301e4d19
ysy9038-eng <ysy9038@gmail.com>
2026-04-06 17:53:39 +0900

    Fix: WebMvcConfig 충돌 해결 및 디자인 브랜치 작업 반영

--- moeats/src/main/java/com/moeats/config/WebMvcConfig.java
+++ moeats/src/main/java/com/moeats/config/WebMvcConfig.java
@@ -7,6 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.moeats.interceptor.LoginCheckInterceptor;
 
+import com.moeats.interceptor.OrderMemberInterceptor;
+import com.moeats.interceptor.OwnerCheckInterceptor;
+import com.moeats.interceptor.PaymentInterceptor;
+import com.moeats.interceptor.RoomMemberInterceptor;
+
+
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
