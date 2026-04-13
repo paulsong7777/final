f5db430b9a37076ff41a8c65bfc3bb8c12a52d02
paulsong7777 <paulsong7777@gmail.com>
2026-04-13 17:01:38 +0900

    main controller에서 user owner 로그인 분기

--- moeats/src/main/java/com/moeats/controller/MainController.java
+++ moeats/src/main/java/com/moeats/controller/MainController.java
@@ -2,13 +2,20 @@ package com.moeats.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import com.moeats.domain.Member;
 
 @Controller
 public class MainController {
 
-	@RequestMapping({"/","/main"})
-	public String main() {
-		
-		return "member/main";
-	}
+    @RequestMapping({"/", "/main"})
+    public String main(@SessionAttribute(name = "member", required = false) Member member) {
+
+        if (member != null && "OWNER".equals(member.getMemberRoleType())) {
+            return "redirect:/owners/dashboard";
+        }
+
+        return "member/main";
+    }
 }
